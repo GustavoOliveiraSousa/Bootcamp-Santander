3e1a10c402f3e325feac5d6413c401f0908f9868
Gustavo <gs98134@gmail.com>
2022-06-21 20:15:15 -0300

    Iniciando Numbers

---
+++ Variáveis e tipos Javascript/Tipos.js
@@ -0,0 +1,25 @@
+//ESTRUTURA DE DADOS
+// let um = "um";
+// um = 1;
+// console.log(um);
+// console.log(typeof um);
+//-------------------------------------------------------
+
+//STRINGS
+// const DIO = 'Digital Inovation One';
+// let name = 'Gustavo Oliveira, ';
+// let job = 'Programador';
+// let resume = `Nome e profissão: ${name} ${job}`;
+// console.log(resume);
+// console.log(resume.length);
+// // console.log(name.concat(job));
+// let exemplo = new String('Beyblade');
+// console.log(exemplo);
+// console.log(typeof exemplo);
+// //-------------------------------------------------------
+
+//NUMBERS
+let num = 100;
+
+console.log(100 +3);
+

b109730a8baddd69f6cc4400ea83e708b9217cc8
Gustavo <gs98134@gmail.com>
2022-06-21 19:34:32 -0300

    Finalizando aula e iniciando tipos

---
+++ Variáveis e tipos Javascript/Atribuindo-valores.js
@@ -0,0 +1,19 @@
+//VARIÁVEIS
+// var firstName = 'Gustavo';
+// var lastName = 'Sousa';
+
+// if (firstName === 'Gustavo') {
+//     var firstName = 'Aléf';
+//     let lastName = 'Sampaio';
+//     lastName = 'Rodrigues';
+//     console.log(firstName, lastName)
+// }
+// console.log(firstName, lastName)
+// ---------------------------------------------------------------------------------
+
+//CONSTANTES
+// FIRST_NAME = "Gustavo";
+// console.log(FIRST_NAME);
+//----------------------------------------------------------------------------------
+
+

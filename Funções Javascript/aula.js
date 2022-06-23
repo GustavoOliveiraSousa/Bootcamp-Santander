bbd2ede9736f8c6d839b19d268a1e9bd3fa77e76
Gustavo <gs98134@gmail.com>
2022-06-23 19:03:03 -0300

    Funções autoinvocáveis e callbacks

---
+++ Funções Javascript/aula.js
@@ -0,0 +1,34 @@
+// //ESTRURURA DE UMA FUNÇÃO
+// function name(params) {
+//     //instruções
+//     return; //valor do retorno
+// }
+// //--------------------------------------------
+
+// //FUNÇÃO ANÔNIMA
+// const soma = function (a, b) { // se chama função anônima, pois ela não tem nome ("anônima")
+//     return a + b;
+// }
+
+// let soma1 = soma(1, 2);
+// let soma2 = soma(5, 4);
+
+// console.log(soma1); //3
+// console.log(soma2); //9
+// //--------------------------------------------
+
+// //FUNÇÃO AUTOINVOCÁVEL E CALLBACKS
+
+// //--------------------------------------------
+
+
+
+// //FUNÇÃO ANÔNIMA
+
+// //--------------------------------------------
+
+
+
+// //FUNÇÃO ANÔNIMA
+
+// //--------------------------------------------

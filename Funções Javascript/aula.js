fc72297809c72a84e5410adfd350f59a7820e792
Gustavo <gs98134@gmail.com>
2022-06-23 19:22:40 -0300

    Funções autoinvocáveis e callbacks

--- Funções Javascript/aula.js
+++ Funções Javascript/aula.js
@@ -17,18 +17,37 @@
 // console.log(soma2); //9
 // //--------------------------------------------
 
-// //FUNÇÃO AUTOINVOCÁVEL E CALLBACKS
+//FUNÇÃO AUTOINVOCÁVEL E CALLBACKS
+(
+    function() {
+        let name = "Gustavo O Programador"
+        return name;
+    }
+)();
+//--------------------------------------------
 
-// //--------------------------------------------
+//CALLBACKS
+//função passada como argumento para outra função
+const calc = function(operacao, num1, num2) {
+    return operacao(num1, num2);
+}
 
+const soma = function(num1, num2) {
+    return num1 + num2;
+}
+
+const sub = function(num1, num2) {
+    return num1 - num2;
+}
+
+const resultSoma = calc(soma, 1, 2);
+const resultSub = calc(sub, 1, 2);
+
+console.log(resultSub);
+console.log(resultSoma);
+//--------------------------------------------
 
 
 // //FUNÇÃO ANÔNIMA
 
-// //--------------------------------------------
-
-
-
-// //FUNÇÃO ANÔNIMA
-
-// //--------------------------------------------
+//--------------------------------------------

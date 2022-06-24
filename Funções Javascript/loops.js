78eade8f4b2d616dbdb87d9aa063c6a9c104b8e2
Gustavo <gs98134@gmail.com>
2022-06-23 21:39:19 -0300

    Else dentro de if

--- Funções Javascript/loops.js
+++ Funções Javascript/loops.js
@@ -1,15 +1,33 @@
-//IS/ELSE
-function numeroPositivo(num) {
-    let resultado;
+// //IF/ELSE
+// function numeroPositivo(num) {
+//     let resultado;
 
-    if (num > 0) {
-        resultado = true;
-    }else{
-        resultado = false;
+//     if (num > 0) {
+//         resultado = true;
+//     }else{
+//         resultado = false;
+//     }
+
+//     return resultado
+// }
+// numero = numeroPositivo(2)
+// console.log(numero);
+// //-------------------------------
+
+//IF/ELSE
+function numeroPositivo(num) {
+    const ehNegativo = num < 0;
+    const maiorQueDez = num >10;
+
+    if (ehNegativo) {
+        return "Esse número é negativo!";
+    }else if (!ehNegativo && maiorQueDez) {
+        return "Esse número é positivo e maior que dez!"
     }
 
-    return resultado
+    return "Esse número é positivo!";
 }
 
-numero = numeroPositivo(2)
+numero = numeroPositivo(50);
 console.log(numero);
+//-------------------------------

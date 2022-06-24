6a8b69c2217f5b5b8b5f06c496e519e961521750
Gustavo <gs98134@gmail.com>
2022-06-23 21:14:40 -0300

    arrays e objetos

--- Funções Javascript/aula/parâmetros.js
+++ Funções Javascript/aula/parâmetros.js
@@ -33,6 +33,24 @@
 // console.log(resultado);
 // //--------------------------------------------------------
 
-//ARRAYS E OBJETOS  
+// //ARRAYS E OBJETOS  
+// //spread: uma forma de lidar separadamente com elementos
+// function sum(y , x , z) {
+//     return x  + y + z;
+// }
+// const numbers = [1, 2, 3];
 
+// console.log(sum(...numbers));
+// //O que era parte de um array se torna um elemento independente
+// //----------------------------------------------------------
+
+//ARRAYS E OBJETOS  
+//rest: combina os argumentos em um array
+function confereTamanho(...args) {
+    console.log(args.length);
+}
+confereTamanho()
+confereTamanho(1, 2)
+confereTamanho(1, 2, 3)
+//O que era um elemento independente se torna parte de um array
 //----------------------------------------------------------

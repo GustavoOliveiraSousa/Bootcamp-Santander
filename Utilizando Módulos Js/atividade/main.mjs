90c3f9ddb567fabdbd598020e4cba4ab9d5d2332
Gustavo <gs98134@gmail.com>
2022-07-06 14:28:55 -0300

    Criação de uma main para chamar o módulo funções

---
+++ Utilizando Módulos Js/atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Gustavo', 20));
+
+console.log(mostraCidade('Gustavo', 'Monsenhor Tabosa'));
+
+console.log(mostraHobby('Gustavo', 'Ouvir música'));

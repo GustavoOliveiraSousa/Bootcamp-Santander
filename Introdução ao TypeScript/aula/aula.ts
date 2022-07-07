c0c7a2a0dbc71df2b518d7db50eaef38f0c15b2d
Gustavo <gs98134@gmail.com>
2022-07-07 18:18:50 -0300

    Como funcionam as interfaces Typescript

--- Introdução ao TypeScript/aula/aula.ts
+++ Introdução ao TypeScript/aula/aula.ts
@@ -1,3 +1,29 @@
-let soma = (a: number, b:number) => a + b;
+//ESCREVENDO A FUÇÃO SOMA EM TYPESCRIPT
+// let soma = (a: number, b:number) => a + b;
+// console.log(soma(2,6));
+//==========================================================
 
-console.log(soma(2,6));
+//COMO FUNCIONAR AS INTERFACES
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico' | 'aéreo';
+    executarRugido(alturaEmDecibeis: number): void;
+}
+interface IFelinos extends IAnimal {
+    visaoNoturna: boolean,
+}
+
+
+const animal: IAnimal = {
+    nome: "Sabiá",
+    tipo: 'aéreo',
+    executarRugido: (alturaEmDecibeis) => ("${alturaEmDecibeis} dB"),
+};
+
+const felino: IFelinos = {
+    nome: "Tigresa",
+    tipo: "terrestre",
+    visaoNoturna: true,
+    executarRugido: (alturaEmDecibeis) => ("${alturaEmDecibeis} dB"),
+}
+//===================================================================

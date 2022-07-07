0f2cd28f8fa3ceb3cfc196c11fe2069d46426168
Gustavo <gs98134@gmail.com>
2022-07-07 18:48:45 -0300

    Aula sobre como funcionam as types no typescript

--- Introdução ao TypeScript/aula/aula.ts
+++ Introdução ao TypeScript/aula/aula.ts
@@ -3,27 +3,59 @@
 // console.log(soma(2,6));
 //==========================================================
 
-//COMO FUNCIONAR AS INTERFACES
-interface IAnimal {
-    nome: string;
-    tipo: 'terrestre' | 'aquatico' | 'aéreo';
-    executarRugido(alturaEmDecibeis: number): void;
-}
-interface IFelinos extends IAnimal {
-    visaoNoturna: boolean,
-}
+// //COMO FUNCIONAR AS INTERFACES
+// interface IAnimal {
+//     nome: string;
+//     tipo: 'terrestre' | 'aquatico' | 'aéreo';
+//     executarRugido(alturaEmDecibeis: number): void;
+// }
+// interface IFelinos extends IAnimal {
+//     visaoNoturna: boolean,
+// }
 
 
-const animal: IAnimal = {
-    nome: "Sabiá",
-    tipo: 'aéreo',
-    executarRugido: (alturaEmDecibeis) => ("${alturaEmDecibeis} dB"),
-};
+// const animal: IAnimal = {
+//     nome: "Sabiá",
+//     tipo: 'aéreo',
+//     executarRugido: (alturaEmDecibeis) => ("${alturaEmDecibeis} dB"),
+// };
 
-const felino: IFelinos = {
-    nome: "Tigresa",
-    tipo: "terrestre",
-    visaoNoturna: true,
-    executarRugido: (alturaEmDecibeis) => ("${alturaEmDecibeis} dB"),
-}
-//===================================================================
+// const felino: IFelinos = {
+//     nome: "Tigresa",
+//     tipo: "terrestre",
+//     visaoNoturna: true,
+//     executarRugido: (alturaEmDecibeis) => ("${alturaEmDecibeis} dB"),
+// }
+// //===================================================================
+
+// //COMO FUNCIONAM AS TYPES
+// //interface serve como um modelo
+// interface IAnimal {
+//     nome: string;
+//     tipo: 'terrestre' | 'aquatico' | 'aéreo';
+//     domestico: boolean;
+// }
+// interface IFelinos extends IAnimal {
+//     visaoNoturna: boolean,
+// }
+
+// interface ICanino extends IAnimal {
+//     porte: "pequeno" | "médio" | "grande",
+// }
+ 
+// //types servem para fazer junções de interfaces
+// type IDomestico = IFelinos | ICanino
+
+// const animal: IDomestico = {
+//     domestico: true,
+//     nome: "Dog",
+//     porte: "grande",
+//     tipo: "terrestre",
+//     visaoNoturna: true,
+// };
+
+// //================================================================
+
+//TRATANDO A TAG INPUT
+
+//====================================================================
--- dist/aula.js
+++ dist/aula.js
@@ -1,3 +1,17 @@
 "use strict";
-var soma = function (a, b) { return a + b; };
-console.log(soma(2, 6));
+//ESCREVENDO A FUÇÃO SOMA EM TYPESCRIPT
+// let soma = (a: number, b:number) => a + b;
+// console.log(soma(2,6));
+//==========================================================
+var animal = {
+    nome: "Sabiá",
+    tipo: 'aéreo',
+    executarRugido: function (alturaEmDecibeis) { return ("${alturaEmDecibeis} dB"); },
+};
+var felino = {
+    nome: "Tigresa",
+    tipo: "terrestre",
+    visaoNoturna: true,
+    executarRugido: function (alturaEmDecibeis) { return ("${alturaEmDecibeis} dB"); },
+};
+//===================================================================

822447a7052052841c39926929a6909e8bc63514
Gustavo <gs98134@gmail.com>
2022-07-09 20:11:08 -0300

    Criação de um script calculadora com html e js

---
+++ Desafio de projeto TypeScript/exemplos/validacao.js
@@ -0,0 +1,14 @@
+//npm install lite-server - para instalar o depurador
+//npm start - para iniciar a depuração
+let button = document.getElementById("button");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+
+function SomaNumeros(num1, num2) {
+    return Number(num1) + Number(num2)
+};
+
+button.addEventListener('click', () => {
+    Saida = (SomaNumeros(input1.value, input2.value));
+    document.getElementById("output").innerHTML= Saida;
+});

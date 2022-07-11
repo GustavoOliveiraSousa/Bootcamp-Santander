02c9e3f9320c559d7d62c59d65d00ec563c3799c
Gustavo <gs98134@gmail.com>
2022-07-11 17:15:09 -0300

    Defininco o tipo de retorno de uma função

--- Desafio de projeto TypeScript/desafio/app.js
+++ Desafio de projeto TypeScript/desafio/app.js
@@ -1,13 +1,5 @@
 "use strict";
-function somaValores(input1, input2) {
-    if (typeof input1 === "string" || typeof input2 === "string") {
-        return input1.toString() + input2.toString();
-    }
-    else {
-        return input1 + input2;
-    }
+function somaValorNumerico(numero1, numero2) {
+    return numero1 + numero2;
 }
-console.log(somaValores(1, 2));
-console.log(somaValores("Que dia é hoje? ", 2));
-console.log(somaValores(1, " biscoito"));
-console.log(somaValores("Olá ", "tudo bem?"));
+console.log(somaValorNumerico(1, 3));
---
+++ Desafio de projeto TypeScript/exemplos/var-perso.ts
@@ -0,0 +1,15 @@
+type input = number | string;
+
+function somaValores(input1: input, input2: input) {
+    if (typeof input1 === "string" || typeof input2 === "string" ) {
+        return input1.toString() + input2.toString();
+    } else{
+        return input1 + input2;
+    }
+}
+
+console.log(somaValores(1, 2));
+console.log(somaValores("Que dia é hoje? ", 2));
+console.log(somaValores(1, " biscoito"));
+console.log(somaValores("Olá ", "tudo bem?"));
+

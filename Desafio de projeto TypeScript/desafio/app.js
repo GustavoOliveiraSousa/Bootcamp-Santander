d9c535828fee7414896b1b3cdf0e137038d10493
Gustavo <gs98134@gmail.com>
2022-07-11 17:47:14 -0300

    Incluindo callback em funções

--- Desafio de projeto TypeScript/desafio/app.js
+++ Desafio de projeto TypeScript/desafio/app.js
@@ -1,8 +1,12 @@
 "use strict";
-// function somaValorNumerico(numero1:number, numero2: number): number {
-//     return numero1 + numero2;
+// function printaValoresNumericos(numero1:number, numero2: number): void {
+//     console.log(numero1 + numero2);
 // }
-// console.log(somaValorNumerico(1 ,3));
-function printaValoresNumericos(numero1, numero2) {
-    console.log(numero1 + numero2);
+function somarValoresNumericosETratar(numero1, numero2, callback) {
+    let resultado = numero1 + numero2;
+    return callback(resultado);
 }
+function aoQuadrado(numero) {
+    return numero * numero;
+}
+console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
---
+++ Desafio de projeto TypeScript/exemplos/void.ts
@@ -0,0 +1,8 @@
+// function somaValorNumerico(numero1:number, numero2: number): number {
+//     return numero1 + numero2;
+// }
+
+// console.log(somaValorNumerico(1 ,3));
+function printaValoresNumericos(numero1:number, numero2: number): void {
+    console.log(numero1 + numero2);
+}

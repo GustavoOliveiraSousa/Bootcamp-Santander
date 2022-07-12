325b14000dba4a3ea1a8b7ce08936c8737a24b8a
Gustavo <gs98134@gmail.com>
2022-07-12 11:31:52 -0300

    tsconfig.json - target

--- Desafio de projeto TypeScript/desafio/app.ts
+++ Desafio de projeto TypeScript/desafio/app.ts
@@ -1,2 +1,11 @@
-// Links Uteis
-let TypeScript = 'https://www.typescriptlang.org/docs/handbook/2/everyday-types.html'
+// npm install tsc-watch --save-dev
+// não ative a configuração outdir no package.json (senão o tsc watch não funciona)
+// npm run "algum script do package.json(voce pode criar) eu configurei o "watch": "tsc --watch" e o "start": "lite-server",
+//tsc --init    - para iniciar o tradutor(cria o arquivo tsconfig.json)
+let buttonTeste = document.getElementById("button");
+
+
+//usano o "?" não precisa usar o if para verificar se existe
+buttonTeste?.addEventListener('click', () =>{
+    console.log("funcionou");
+})
---
+++ Desafio de projeto TypeScript/exemplos/links-uteis.ts
@@ -0,0 +1,2 @@
+// Links Uteis
+let TypeScript = 'https://www.typescriptlang.org/docs/handbook/2/everyday-types.html'

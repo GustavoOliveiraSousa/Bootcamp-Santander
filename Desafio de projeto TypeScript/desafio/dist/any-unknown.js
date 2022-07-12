ce761f8a929cbfb81522782a55548ce41a5e11f1
Gustavo <gs98134@gmail.com>
2022-07-12 12:26:15 -0300

    Organizando a pasta

---
+++ Desafio de projeto TypeScript/desafio/dist/any-unknown.js
@@ -0,0 +1,17 @@
+"use strict";
+// let anyEstaDeVolta: any;
+// anyEstaDeVolta = 3;
+// anyEstaDeVolta = "Olá";
+// anyEstaDeVolta = 5;
+// let stringTeste: string = "Verificar";
+// stringTeste = anyEstaDeVolta;
+// let unknownValue: unknown;
+// unknownValue = 1;
+// unknownValue = "Oi";
+// unknownValue = true;
+// unknownValue = "vai sim"
+// let stringTeste2: string = "agora vai";
+// if ( typeof unknownValue === 'string') {
+//     //só posso reatribuir um tipo unknown se eu verificar com um if
+//     stringTeste2 = unknownValue;
+// }
---
+++ Desafio de projeto TypeScript/desafio/dist/tipo-retorno.js
@@ -0,0 +1,5 @@
+"use strict";
+function somaValorNumerico(numero1, numero2) {
+    return numero1 + numero2;
+}
+console.log(somaValorNumerico(1, 3));

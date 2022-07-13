6c08b8cce4bf3099e3d0d523d7cddb0e91f084d7
Gustavo <gs98134@gmail.com>
2022-07-13 14:43:30 -0300

    Instalando o Angular via npm

--- Introducao-ao-Angular8/Course-manager/karma.conf.js
+++ Introducao-ao-Angular8/Course-manager/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/coust-manage'),
+      dir: require('path').join(__dirname, './coverage/course-manager'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Introducao-ao-Angular8/Course-manager/src/app/app.component.spec.ts
+++ Introducao-ao-Angular8/Course-manager/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coust-manage'`, () => {
+  it(`should have as title 'Course-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coust-manage');
+    expect(app.title).toEqual('Course-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('coust-manage app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Course-manager app is running!');
   });
 });

07193716df36aef083f0904d0ca9f5e6e0f2b709
Gustavo <gs98134@gmail.com>
2022-07-27 16:25:40 -0300

    Desenvolvendo o serviço PeopleService

--- Angular-to-do-list/crud-dio-angular-main/crud-dio-angular-main/src/app/app.component.ts
+++ Angular-to-do-list/crud-dio-angular-main/crud-dio-angular-main/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PeopleService } from './shared/services/people.service';
 
 @Component({
   selector: 'app-root',
@@ -8,39 +9,33 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   count: number  = 0;
   nome: string = 'Gustavo Oliveira de Sousa';
-  text = ''
+  text = '';
 
-  pessoas = [
-    {
-      nome: 'Gustave',
-      sobrenome: 'Eiffel'
-    },
-    {
-      nome: 'Galápagos',
-      sobrenome: 'Fetus'
-    },
-    {
-      nome: 'Hércules',
-      sobrenome: 'Olimpio'
-    },
-    {
-      nome: 'Apolo',
-      sobrenome: 'Notus'
-    },
-  ];
+  pessoas: any [] = [];
 
-  constructor() {}
+  constructor(private peopleService: PeopleService) {
+    
+  }
 
   ngOnInit(){
+    this.getPeople();
+
     let interval = setInterval(() => {
       this.count++; 
       if (this.count == 10){
         clearInterval(interval);
       }
-    }, 1000)
+    }, 1000);
+
   }
 
   clicou(nome: string): void {
     console.log('Clicou em mim', nome);
+  }
+
+  getPeople(){
+    this.peopleService.getPeople().subscribe(people =>{
+      this.pessoas = people;
+    })
   }
 }
--- Angular-to-do-list/crud-dio-angular-main/crud-dio-angular-main/src/app/diretivas/marca-texto.directive.spec.ts
+++ Angular-to-do-list/crud-dio-angular-main/crud-dio-angular-main/src/app/diretivas/marca-texto.directive.spec.ts
@@ -1,8 +1,9 @@
-import { MarcaTextoDirective } from './marca-texto.directive';
+// import { MarcaTextoDirective } from "./marca-texto.directive";
 
-describe('MarcaTextoDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MarcaTextoDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+
+// describe('MarcaTextoDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new MarcaTextoDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });

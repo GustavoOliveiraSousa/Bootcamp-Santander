ce62a4dfa63afe6f4f1f5adb25297a9727edf411
Gustavo <gs98134@gmail.com>
2022-07-27 22:06:21 -0300

    Organizando o repositório

--- Desafio de projeto Typescript projeto/Desafios/desafio-01.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let employer = {
-    code: 10,
-    name: "John"
-};
-console.log(employer);
--- Desafio de projeto Typescript projeto/Desafios/desafio-01.ts
+++
@@ -1,6 +0,0 @@
-let employer = {
-    code: 10,
-    name: "John"
-}
-
-console.log(employer);

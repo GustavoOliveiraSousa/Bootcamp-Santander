77ab5450c2f90e22bb0585cbf1730646973a356d
Gustavo <gs98134@gmail.com>
2022-08-04 20:47:24 -0300

    Exercitanto 01 - conceituação e criação

---
+++ JAVA/Exercicios- variaveis-tipos-operadores/src/Exercitando01/Main.java
@@ -0,0 +1,32 @@
+package Exercitando01;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        int I;
+        /*int I;*/
+        int i1;
+        int _la;
+        /*int $aq;*/
+
+        i = 5;
+        I = 10;
+        int cwzlciugl76786;
+        int xh4çcuh_947;
+        int g4lx987et96_igf3 = 10;
+
+        /*t87dtf85= 100;
+        t87dtf8_hf5= 100;*/
+        final int NUMERO_TENTATIVAS = 5;
+//        final int numeroTentativas = 5;
+
+        System.out.println(i);
+        System.out.println(I);
+//        System.out.println(_la);
+//        System.out.println($aq);
+
+        System.out.println(g4lx987et96_igf3);
+        System.out.println(NUMERO_TENTATIVAS);
+
+    }
+}

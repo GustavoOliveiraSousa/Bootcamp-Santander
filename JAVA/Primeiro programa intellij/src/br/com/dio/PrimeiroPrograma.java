e54706c521b5a1d6b441cd3771a78152a1a0b271
Gustavo <gs98134@gmail.com>
2022-08-04 15:43:32 -0300

    First commit Intellij aula DIO

---
+++ JAVA/Primeiro programa intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       Gato gato = new Gato("Xilito", "marrom", 12);
+        System.out.println(gato);
+
+        /*int a = 1000;
+        int b = 1022;
+        System.out.println("Hello Mars!!! "+ (a+b));*/
+    }
+}

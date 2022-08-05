4ebfa65dbc1d056ba17c526e4f158520a9f6789d
Gustavo <gs98134@gmail.com>
2022-08-04 21:41:38 -0300

    Exercitanto 02 - tipos de dado

---
+++ JAVA/Exercicios- variaveis-tipos-operadores/src/Exercitando02/Main.java
@@ -0,0 +1,52 @@
+package Exercitando02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+//        short s2 = 40000;
+
+//        int i1 = -100000000000000;
+        int i2 = 28500;
+
+//        long l1 = 10000000000000000000000L;
+//        long l2 = 20034003300405505005L;
+
+//        float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.04d;
+
+        String c1 = "G";
+//        char c2 = "Tm";
+//        char c3 = "\u0057";
+
+        String st1 = "Gustavo";
+        String st2 = "Oliveira";
+//        String st3 = "kjbxç4xgc çkrcbçh4v çx4hkcçi";
+
+        String dt1 = "04/08/2022";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(f2);
+        System.out.println(b2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(dt1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+    }
+}
